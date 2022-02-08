eee2d862b022dc869b8764727b9621649a5a6985
Nikolay Nedelkov <nikolay.nedelkov@playtech.com>
2022-02-08 18:05:29 +0200

    initial commit, skeleton of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {ServersComponent} from './servers/servers.component';
+import {UsersComponent} from './users/users.component';
+import {UserComponent} from './users/user/user.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {ServerComponent} from './servers/server/server.component';
+import {ServersService} from './servers/servers.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ServersComponent,
+        UsersComponent,
+        UserComponent,
+        EditServerComponent,
+        ServerComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [ServersService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
